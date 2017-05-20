0cad6e5abbbb9b6f52a607ba4be752b78aeac6e0
ehacinom <ehacinom@gmail.com>
2017-05-20 14:22:21 -0500

    yay!

--- src/minimax.js
+++ src/minimax.js
@@ -56,8 +56,8 @@ const makeMove = (state) => {
 		// This variable gets handed down in the recursive
 		// minimax call unchanged.
 
-		const stateValue = minimax(potentialState, depth, playerMoving);
-		//const stateValue = minimaxAlphaBetaWrapper(potentialState, depth, playerMoving)
+        // const stateValue = minimax(potentialState, depth, playerMoving);
+        const stateValue = minimaxAlphaBetaWrapper(potentialState, depth, playerMoving)
 
 		if (stateValue > bestMoveValue || bestMoveValue === null){
 			bestMoveIndex = i;
@@ -93,6 +93,9 @@ You'll want to pass the tests defined in minimax_specs.js.
 */
 const heuristic = (state, maximizingPlayer) => {
 
+    // console.log('==================')
+    // console.log(state)
+
 	//This is how you can retrieve the minimizing player.
     const minimizingPlayer = (maximizingPlayer == 'x') ? 'o' : 'x';
 
@@ -100,7 +103,16 @@ const heuristic = (state, maximizingPlayer) => {
     const linesOfLengthTwoForX = state.numLines(2, 'x')
 
     //Your code here.  Don't return random, obviously.
-	return Math.random()
+	let h = 0;
+    for (let i = state.winning; i > 1; i--) {      
+        h += i*i*i*i*i * (
+            state.numLines(i, maximizingPlayer) - 
+            state.numLines(i, minimizingPlayer)
+        )
+    }
+    
+    // console.log(h);
+    return h;
 }
 
 
@@ -125,14 +137,29 @@ which returns whether the next moving player is 'x' or 'o',
 to see if you are maximizing or minimizing.
 */
 const minimax = (state, depth, maximizingPlayer) => {
-	var minimizingPlayer = (maximizingPlayer == 'x') ? 'o' : 'x';
+    // console.log('minimax', depth)
 	var possibleStates = state.nextStates();
 	var currentPlayer = state.nextMovePlayer;
-	//Your code here.
-	return Math.random();
+    
+    if (depth === 0 || !possibleStates.length)
+        return heuristic(state, maximizingPlayer);
+        
+    if (currentPlayer === maximizingPlayer) {
+        let x = possibleStates.reduce((max, curState) => {
+            let cur = minimax(curState, depth-1, maximizingPlayer)
+            return (max > cur) ? max : cur;
+        }, -10000000);
+        
+        return x
+    } else {
+        let x = possibleStates.reduce((min, curState) => {
+            let current = minimax(curState, depth-1, maximizingPlayer)
+            return (min > current) ? current : min;
+        }, 10000000);
+        
+        return x
+    }
 }
-
-
 
 /* minimaxAlphaBetaWrapper is a pre-written function, but it will not work
    unless you fill in minimaxAB within it.
@@ -154,9 +181,37 @@ const minimaxAlphaBetaWrapper = (state, depth, maximizingPlayer) => {
     does; this is why it is a very high value to start with.
 	*/
 	const minimaxAB = (state, depth, alpha, beta) => {
+        // console.log('minimax', depth)
+    	var possibleStates = state.nextStates();
+    	var currentPlayer = state.nextMovePlayer;
+    
+        if (depth == 0 || !possibleStates.length)
+            return heuristic(state, maximizingPlayer);
+        
+        if (currentPlayer === maximizingPlayer) {
+            let x = possibleStates.reduce((max, curState) => {
+                alpha = (max > alpha) ? max : alpha;
+                if (alpha > beta) return max;
+                
+                let cur = minimax(curState, depth-1, maximizingPlayer)
+                return (max > cur) ? max : cur;
+            }, -10000000);
+        
+            return x
+        } else {
+            let x = possibleStates.reduce((min, curState) => {
+                beta = (min < beta) ? min : beta;
+                if (alpha > beta) return min;
+                
+                let current = minimax(curState, depth-1, maximizingPlayer)
+                return (min > current) ? current : min;
+            }, 10000000);
+        
+            return x
+        }
 	}
 
-	return minimaxAB(state, depth, -100000,100000);
+	return minimaxAB(state, depth, -100000, 100000);
 }
 
 module.exports = {makeMove, minimax, heuristic};
